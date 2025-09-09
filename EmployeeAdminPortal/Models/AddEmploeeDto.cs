9dd333537765a7a754db90cb2f344588725d9a2e
Serhii Naku <naku.serega@gmail.com>
2025-09-09 19:59:19 +0300

    Delete forecast Correct email type

--- EmployeeAdminPortal/Models/AddEmploeeDto.cs
+++ EmployeeAdminPortal/Models/AddEmploeeDto.cs
@@ -6,7 +6,7 @@ namespace EmployeeAdminPortal.Models
     public class AddEmploeeDto
     {
         public required string Name { get; set; }
-        public required string? Email { get; set; }
+        public required string Email { get; set; }
         public string? Phone { get; set; }
         public int Salary { get; set; }
     }
