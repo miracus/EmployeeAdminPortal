e043797352cbd64e28b5c72d04bf23a755402657
Serhii Naku <naku.serega@gmail.com>
2025-09-10 23:45:57 +0300

    Migration Other Laptop

---
+++ EmployeeAdminPortal/Migrations/20250910203315_AddIsDeletedAndRenameIdToEmployeeId.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeAdminPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDeletedAndRenameIdToEmployeeId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
